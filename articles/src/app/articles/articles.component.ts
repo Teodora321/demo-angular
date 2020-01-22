613ef4e30e7af0b284c0ddc986a10e378cf4c345
Teodora321 <teodoraakehaiovaa@gmail.com>
2020-01-22 21:41:19 +0200

    DI

--- articles/src/app/articles/articles.component.ts
+++ articles/src/app/articles/articles.component.ts
@@ -1,20 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Article } from '../models/article.model';
 import { ArticleData } from '../data/data';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
-  styleUrls: ['./articles.component.css']
+  styleUrls: ['./articles.component.css'],
 })
-export class ArticlesComponent implements OnInit {
+  
+export class ArticlesComponent {
 
   articles: Article[];
   
-  constructor() { }
-
-  ngOnInit() {
-    this.articles = new ArticleData().getData();
+  constructor(private articleData: ArticleData) {
+    this.articles = articleData.getData();
   }
 
 }
